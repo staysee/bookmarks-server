2b5c0b49ba39836bfb7070b2d92349a3e55baa60
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-03 19:33:15 -0800

    migration for bookmarks and bookmarks-test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Bookmarks Server
+
+## Migrate
+`npm run migrate`
+
+## Seed
+To seed the database with the script, use the following command:
+`psql -U dunder_mifflin -d bookmarks -f ./seeds/seed.bookmarks.sql`
---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('Thinkful', 'https://www.thinkful.com', 'The Thinkful website', '5'),
+  ('Netflix', 'https://www.netflix.com', 'Where you can watch movies', '5'),
+  ('Nordstrom Rack', 'https://www.nordstromrack.com', 'Nordstrom stuff cheaper', '5'),
+  ('Spotify', 'https://www.spotify.com', 'Music player', '3'),
+  ('LinkedIn', 'https://www.linkedin.com', 'Linkedin site', '2'),
+  ('Google', 'https://www.google.com', 'search on google', null),
+  ('Hulu', 'https://www.hulu.com', 'another place to watch movies and shows', '5'),
+  ('Facebook', 'https://www.facebook.com', 'social network', '5'),
+  ('Sephora', 'https://www.sephora.com', 'Shop for makeup', '4'),
+  ('YouTube', 'https://www.youtube.com', 'watch videos', '4');
