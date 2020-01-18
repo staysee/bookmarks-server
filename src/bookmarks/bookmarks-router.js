0d9fd2b852279e94a351f1ef864bf365f2f431bf
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-17 18:49:35 -0800

    patch tests passing

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const { isWebUri } = require('valid-url')
 const xss = require('xss')
@@ -27,47 +28,6 @@ bookmarksRouter
             })
             .catch(next)
     })
-    // .post(bodyParser, (req, res, next) => {
-    //     const { title, url, description, rating } = req.body;
-    //     const newBookmark = { title, url, description, rating }
-        
-    //     for (const [key, value] of Object.entries(newBookmark)){
-    //         if (value == null)
-    //             return res.status(400).json({
-    //                 error: { message: `Missing '${key}' in request body`}
-    //             })
-    //     }
-
-    //     const ratingNum = Number(rating)
-
-    //     if (!Number.isInteger(ratingNum) || Number(ratingNum)<0 || Number(ratingNum)>5 ){
-    //         logger.error(`Invalid rating: ${rating}`)
-    //         return res
-    //             .status(400)
-    //             .send({
-    //                 error: { message: `Invalid rating`}
-    //             })
-    //     }
-
-    //     if (!isWebUri(url)) {
-    //         logger.error(`Invalid url '${url}' supplied`)
-    //         return res.status(400).send({
-    //           error: { message: `Invalid url` }
-    //         })
-    //       }
-
-
-    //     const knexInstance = req.app.get('db')
-    //     BookmarksService.insertBookmark(knexInstance, newBookmark)
-    //         .then(bookmark => {
-    //             logger.info(`Bookmark with id ${bookmark.id} created.`)
-    //             res
-    //                 .status(201)
-    //                 .location(`/bookmarks/${bookmark.id}`)
-    //                 .json(serializeBookmark(bookmark))
-    //         })
-    //         .catch(next)
-    // })
     .post(bodyParser, (req, res, next) => {
         for (const field of ['title', 'url', 'rating']) {
           if (!req.body[field]) {
@@ -106,7 +66,7 @@ bookmarksRouter
             logger.info(`Bookmark with id ${bookmark.id} created.`)
             res
               .status(201)
-              .location(`/bookmarks/${bookmark.id}`)
+              .location(path.posix.join(req.originalUrl, `${bookmark.id}`))
               .json(serializeBookmark(bookmark))
           })
           .catch(next)
@@ -114,6 +74,22 @@ bookmarksRouter
 
 bookmarksRouter
     .route('/:id')
+    .all((req, res, next) => {
+        const { id } = req.params
+        const knexInstance = req.app.get('db')
+        BookmarksService.getById(knexInstance, id)
+            .then(bookmark => {
+                if (!bookmark){
+                    logger.error(`Bookmark with id ${id} not found.`)
+                    return res.status(404).json({
+                        error: { message: `Bookmark doesn't exist` }
+                    })
+                }
+                res.bookmark = bookmark //save the article for the next middlware
+                next() //don't forget to call next so the next middleware happens
+            })
+            .catch(next)
+        })
     .get((req, res, next) => {
         const { id } = req.params
         const bookmark = bookmarks.find( b => b.id == id)
@@ -140,6 +116,18 @@ bookmarksRouter
                 res.status(204).end()
             })
             .catch(next)
+    })
+    .patch(bodyParser,(req, res, next) => {
+        const { title, url, description, rating } = req.body
+        const bookmarkToUpdate = { title, url, description, rating }
+
+        BookmarksService.updateBookmark(
+            req.app.get('db'), req.params.id, bookmarkToUpdate
+        )
+        .then(numRowsAfected => {
+            res.status(204).end()
+        })
+        .catch(next)
     })
 
 module.exports = bookmarksRouter
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -286,4 +286,51 @@ describe(`Bookmarks Endpoints`, function() {
             })
         })
     })
+
+    describe.only(`PATCH /api/bookmarks/:id`, () => {
+        context(`Given no bookmarks`, () => {
+            it(`responds with 404`, () => {
+                const bookmarkId = 123456
+                return supertest(app)
+                    .patch(`/api/bookmarks/${bookmarkId}`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(404, { error: { message: `Bookmark doesn't exist`}})
+            })
+        })
+
+        context(`Given there are bookmarks in the database`, () => {
+            const testBookmarks = makeBookmarksArray()
+
+            beforeEach('insert bookmarks', () => {
+                return db
+                    .into('bookmarks')
+                    .insert(testBookmarks)
+            })
+
+            it(`responds with 204 and updates the bookmark`, () => {
+                const idToUpdate = 2
+                const updateBookmark = {
+                    title: 'updated bookmark',
+                    url: 'http://updated-url.com',
+                    description: 'updated description',
+                    rating: 4
+                }
+                const expectedBookmark = {
+                    ...testBookmarks[idToUpdate-1],
+                    ...updateBookmark
+                }
+                return supertest(app)
+                    .patch(`/api/bookmarks/${idToUpdate}`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .send(updateBookmark)
+                    .expect(204)
+                    .then(res =>
+                        supertest(app)
+                            .get(`/api/bookmarks/${idToUpdate}`)
+                            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                            .expect(expectedBookmark)
+                    )
+            })
+        })
+    })
 })
